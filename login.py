c9ed86af306dfa182bf647c00fb689c7e9eb9d12
Toph-earth <bramsgee@gmail.com>
2026-07-16 15:07:40 +0530

    Update: added more robust authentication to login feature

--- login.py
+++ login.py
@@ -5,6 +5,18 @@ credentials = {
 def login(username, password):
     if credentials["username"] == username and credentials["pw"] == password:
         print(f"User {username} logged in successfully")
+    elif credentials["username"] == username and credentials["pw"] != password:
+        print("Incorrect password. Retry login")
+    elif credentials["username"] != username and credentials["pw"] == password:
+        print("Incorrect username. Retry login")
+    else:
+        print("Incorrect username and password. Retry login")
 
 def logout(username):
     print(f"User {username} has logged out successfully!")
+
+print("\t\tWelcome to Hospital Appointment Booking System\n\t\tPlease login")
+user = input("Enter username: ")
+pw = input("Enter password: ")
+
+login(user, pw)
